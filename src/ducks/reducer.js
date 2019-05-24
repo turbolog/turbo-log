5634a3b8f73bbd47e6611d297e9ba77bd71a64f7
David Hernandez <djhernandez@Davids-MacBook-Air.local>
2019-05-24 15:21:40 -0500

    basic redux store, imported axios, redux, react-redux, promise middleware

---
+++ src/ducks/reducer.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const initialState = {
+  username: ""
+};
+
+//action types
+
+//action creators
+
+//reducer
+export default function authReducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
