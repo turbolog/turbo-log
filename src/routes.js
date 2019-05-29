38cbeb10063ae97a261e37e21f676636ec3484fd
David Hernandez <djhernandez@Davids-MacBook-Air.local>
2019-05-29 15:17:45 -0500

    added single/multi car garage

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,13 @@ import Home from "./components/home/Home";
 import Forum from "./components/forum/Forum";
 import Register from "./components/register/Register";
 import Login from "./components/login/Login";
+import SingleCarGarage from "./components/garage/SingleCarGarage";
+import MultiCarGarage from "./components/garage/MultiCarGarage";
 
 export default (
   <Switch>
+    <Route path="/garage/multi" component={MultiCarGarage} />
+    <Route path="/garage/single" component={SingleCarGarage} />
     <Route path="/login" component={Login} />
     <Route path="/register" component={Register} />
     <Route path="/forum" component={Forum} />
