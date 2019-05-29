badfde2eb2f9dd472c92138ff059d624b8caf507
neaj mahmud <neajmahmud230@gmail.com>
2019-05-29 09:41:28 -0500

    setup account

--- src/components/forum/Forum.js
+++ src/components/forum/Forum.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import Grid from '@material-ui/core/Grid';
-import GridList from '@material-ui/core/GridList';
+
 import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
+
 import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import Collapse from '@material-ui/core/Collapse';
@@ -15,19 +14,13 @@ import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import red from '@material-ui/core/colors/red';
 import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
 import MoreVertIcon from '@material-ui/icons/MoreVert';
-import Container from '@material-ui/core/Container';
 import AddComment from '@material-ui/icons/AddComment';
 import TextField from '@material-ui/core/TextField';
-import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
-import Checkbox from '@material-ui/core/Checkbox';
-import Favorite from '@material-ui/icons/Favorite'
-import FormControlLabel from '@material-ui/core/FormControlLabel';
 import "./Forum.css"
-import { FormGroup } from '@material-ui/core';
 import NavBar from '../reusable/NavBar';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles(theme => ({
     // card: {
@@ -153,7 +146,11 @@ const Forum =() => {
         margin="dense"
         onChange={handleComment}
       />
+      
         </CardContent>
+        <Button variant="contained" color="primary" style={{margin:"0 0 10px 10px"}} >
+        Add
+      </Button>
       </Collapse>
       </Card>
 
--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,11 @@ import Home from "./components/home/Home";
 import Forum from "./components/forum/Forum";
 import Register from "./components/register/Register";
 import Login from "./components/login/Login";
+import Account from "./components/account/Account";
 
 export default (
   <Switch>
+    <Route path="/account" component={Account} />
     <Route path="/login" component={Login} />
     <Route path="/register" component={Register} />
     <Route path="/forum" component={Forum} />
