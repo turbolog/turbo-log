8e1788a4303ad4b0144f4b765ba7ab0fe74cb044
Edson Silva-Filho <edsonsilva@Edsons-MacBook-Pro.local>
2019-05-31 14:41:57 -0500

    comments accessable

--- server/index.js
+++ server/index.js
@@ -6,8 +6,8 @@ const massive = require("massive");
 //Controller File Imports
 const { register,login,logout,getSession } = require("./controller/authCtrl")
 const { getUserGarage, updateMiles, deleteVehicle, addVehicle } = require("./controller/vehicleCtrl")
-const { getRecords, addRecord, updateRecord, deleteRecord } = require("./controller/recordCtrl")
-const { getPosts, addPost, deletePost } = require("./controller/postCtrl")
+const { getRecords, addRecord, updateRecord, deleteRecord, getVehicleRecord } = require("./controller/recordCtrl")
+const { getPosts, addPost, deletePost, getComments, addComment, getFavorites, getUserPosts } = require("./controller/postCtrl")
  
 const app = express()
 app.use(express.json())
@@ -36,18 +36,20 @@ app.put("/api/vehicles/:vehicle_id", updateMiles)
 app.post("/api/vehicles", addVehicle)
 app.delete("/api/vehicles/:vehicle_id", deleteVehicle)
 
-//Vehicle Endpoints
+//Record Endpoints
 app.get("/api/records", getRecords)
 app.post("/api/records", addRecord)
 app.put("/api/records/:report_id", updateRecord)
 app.delete("/api/records/:report_id", deleteRecord)
+app.get("/api/vehicles/records/:vehicle_id", getVehicleRecord)
 
 //Post Endpoints
 app.get("/api/posts", getPosts)
+app.get("/api/users/posts", getUserPosts)
 app.post("/api/posts", addPost)
 app.delete("/api/posts/:post_id", deletePost)
-app.put("/api/favorite")
-app.post("/api/comments")
-app.put("/api/")
+app.get("/api/favorites", getFavorites)
+app.get("/api/comments/:post_id", getComments)
+app.post("/api/comments/:post_id", addComment)
 
 app.listen(SERVER_PORT, () => console.log(`Listening on ${SERVER_PORT}`))
