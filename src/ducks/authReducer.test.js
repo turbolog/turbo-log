96195ce29072c128a7ac31a528fa8e9599011639
David Hernandez <djhernandez@Davids-MacBook-Air.local>
2019-06-05 13:46:15 -0500

    davids tests done

--- src/ducks/authReducer.test.js
+++ src/ducks/authReducer.test.js
@@ -1,3 +1,29 @@
-const { update, logout, login } = require("./authReducer");
+const {
+  update,
+  register,
+  login,
+  logout,
+  getUserSession
+} = require("./authReducer");
 
-const 
+
+//David's Unit Tests
+describe("authReducer functions return corresponding action type", () => {
+  test("login() returns the right Type", () => {
+    expect(login().type).toEqual("LOGIN");
+  });
+  test("update() returns the right Type", () => {
+    expect(update().type).toEqual("UPDATE");
+  });
+  test("logout() returns the right Type", () => {
+    expect(logout().type).toEqual("LOGOUT");
+  });
+  test("register() returns the right Type", () => {
+    expect(register().type).toEqual("REGISTER");
+  });
+  test("getUserSession() returns the right Type", () => {
+    expect(getUserSession().type).toEqual("GET_USER_SESSION");
+  });
+});
+
+//David's E2E Tests
